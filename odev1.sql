fe3e75610765711c38bb5e409876dd0b2b8bca55
oguzhanbbtest <105600982+oguzhanbbtest@users.noreply.github.com>
2022-06-02 14:01:02 +0300

    Update odev1.sql

--- odev1.sql
+++ odev1.sql
@@ -11,4 +11,4 @@ select first_name, last_name from customer
 where first_name = 'Mary' ;
 --film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
 select * from film
-where length <= 50 and (not rental_rate = 2.99 or rental_rate = 4.99) ;
+where length <= 50 and ( NOT rental_rate = 2.99 or rental_rate = 4.99) ;
