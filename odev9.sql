b725ae073dce1d465261b641d4b2629db9adf812
Oğuzhan Bahadır Bayram <105600982+oguzhanbbtest@users.noreply.github.com>
2022-06-07 15:25:25 +0300

    Create odev9.sql

---
+++ odev9.sql
@@ -0,0 +1,19 @@
+###https://app.patika.dev/courses/sql/Odev9 ###
+
+--city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country)
+---isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT * FROM city
+INNER JOIN country ON city.country_id = country.country_id;
+
+--customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki 
+---first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT payment_id, first_name, last_name FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id ;
+
+--customer tablosu ile rental tablosunda bulunan rental_id ile customer 
+---tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT rental_id, first_name, last_name FROM customer
+INNER JOIN rental ON rental.customer_id = customer.customer_id ;
